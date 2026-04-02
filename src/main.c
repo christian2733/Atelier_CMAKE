d140a485822fad5f619179ea2646b04b98b0f329
freddy2706 <m.christianfreddy@ecoles-epsi.net>
2026-04-02 13:10:48 +0200

    Refactor calculator operations into separate functions
    Refactored main function to separate arithmetic operations into distinct functions.

--- src/main.c
+++ src/main.c
@@ -1,35 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "./../include/calculator.h"
+#include "./../../include/calculator.h"
 
-int main(int argc, char *argv[]){
-	if (argc == 4)
-	{
-	char* op = argv[1];
-	char* a = argv[2];
-	char* b = argv[3];
-	double r = 0;
-	if (strcmp(op, "add") == 0) {
-		r = _add(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "sub") == 0) {
-		r = _sub(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "mul") == 0) {
-		r = _mul(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "div") == 0) {
-		r = _div(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else {printf("Erreur de parametres");}
-	}
-	else {printf("Erreur de parametres");}
-	return 0;
+double _add(double a, double b) {
+	return a+b;
 }
 
-		
+double _sub(double a, double b) {
+	return a-b;
+}
+
+double _mul(double a, double b) {
+	return a*b;
+}
+
+double _div(double a, double b) {
+	return a/b;
+}
+	
