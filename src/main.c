b2a616e1a1777c8ddd46defeeeff6a1f0226001f
freddy2706 <m.christianfreddy@ecoles-epsi.net>
2026-04-02 13:12:28 +0200

    Refactor main function for calculator operations 2
    Refactored main function to handle multiple operations and added error handling for parameters.

--- src/main.c
+++ src/main.c
@@ -1,18 +1,45 @@
-#include "./../../include/calculator.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../../include/calculator.h"
 
-double _add(double a, double b) {
-	return a+b;
-}
+int main(int argc, char *argv[]){
+    if (argc >= 3) // On accepte 3 arguments (car) ou 4 (add, sub...)
+    {
+        char* op = argv[1];
+        char* a = argv[2];
+        double r = 0;
 
-double _sub(double a, double b) {
-	return a-b;
+        if (strcmp(op, "add") == 0 && argc == 4) {
+            char* b = argv[3];
+            r = _add(atof(a), atof(b));
+            printf("%lf", r);
+        }
+        else if (strcmp(op, "sub") == 0 && argc == 4) {
+            char* b = argv[3];
+            r = _sub(atof(a), atof(b));
+            printf("%lf", r);
+        }
+        else if (strcmp(op, "mul") == 0 && argc == 4) {
+            char* b = argv[3];
+            r = _mul(atof(a), atof(b));
+            printf("%lf", r);
+        }
+        else if (strcmp(op, "div") == 0 && argc == 4) {
+            char* b = argv[3];
+            r = _div(atof(a), atof(b));
+            printf("%lf", r);
+        }
+        else if (strcmp(op, "car") == 0) { // Nouvelle fonction !
+            r = _car(atof(a));
+            printf("%lf", r);
+        }
+        else {
+            printf("Erreur de parametres");
+        }
+    }
+    else {
+        printf("Erreur de parametres");
+    }
+    return 0;
 }
-
-double _mul(double a, double b) {
-	return a*b;
-}
-
-double _div(double a, double b) {
-	return a/b;
-}
-	
